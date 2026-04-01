eba53bc17d0653def860bd5691786a9d728ef30b
Theophilus K. Dadzie <98459253+Tphilus@users.noreply.github.com>
2026-04-01 08:46:29 -0700

    Time: 119 ms (53.58%), Space: 27 MB (78.03%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -3,11 +3,11 @@ class Solution:
         nums.sort()
         result = 0
         up = 0
-        
+
         for i in range(1, len(nums)):
             if nums[i] != nums[i - 1]:
                 up += 1
-                
+            
             result += up
         
         return result
